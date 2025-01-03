48dfce9016190741da8b013f3bb54a2f575cd058
Danieltorr2 <dandoesgrowth@gmail.com>
2025-01-03 13:37:38 -0600

    Initial Commit

---
+++ FairShare/FairShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FairShareApp.swift
+//  FairShare
+//
+//  Created by Daniel Torres on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FairShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FairShareTests/FairShareTests.swift
@@ -0,0 +1,16 @@
+//
+//  FairShareTests.swift
+//  FairShareTests
+//
+//  Created by Daniel Torres on 1/3/25.
+//
+
+import Testing
+
+struct FairShareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
